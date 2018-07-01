f1ac263ab66e954120e2a4aa45bbe3ffa8deb4f1
mkomakonkon <34379432+mkomakonkon@users.noreply.github.com>
2018-07-01 13:58:20 +0900

    Add files via upload

---
+++ nano_MIDI_YMZ294_DUAL/libraries/YMZ294nano3/YMZ294nano3.cpp
@@ -0,0 +1,279 @@
+/*
+インパクトドリルさんのYMZ294ライブラリ
+http://wp.hrmux.com/?page_id=991
+を修正し、MIDI対応、YMZ294x2個対応しています。
+Ver3:高速化のため全てポート制御に変更
+*/
+
+#include "YMZ294nano3.h"
+
+// MML char to Freqyency Convert Function
+word CalcFreqByMML(byte octave, char note, bool is_sharp)
+{
+  int pos = 0;
+  switch (note) {
+    case 'C':
+    case 'c':
+      break;
+    case 'D':
+    case 'd':
+      pos = 2;
+      break;
+    case 'E':
+    case 'e':
+      pos = 4;
+      break;
+    case 'F':
+    case 'f':
+      pos = 5;
+      break;
+    case 'G':
+    case 'g':
+      pos = 7;
+      break;
+    case 'A':
+    case 'a':
+      pos = 9;
+      break;
+    case 'B':
+    case 'b':
+      pos = 11;
+      break;
+    default:
+      break;
+  }
+  return noteFreq[octave * 12 + pos + (is_sharp ? 1 : 0)];
+}
+
+/* ------------------------------------------------------------------------ */
+// Constructor
+/* ------------------------------------------------------------------------ */
+YMZ294nano3::YMZ294nano3(byte wrcs0_pin, byte wrcs1_pin, byte a0_pin, byte reset_pin)
+ : WRCS0_PIN(wrcs0_pin), WRCS1_PIN(wrcs1_pin), A0_PIN(a0_pin), RESET_PIN(reset_pin)
+{
+	param_reset();
+}
+
+/* ------------------------------------------------------------------------ */
+// Public Functions
+/* ------------------------------------------------------------------------ */
+void YMZ294nano3::Reset(void)
+{
+	param_reset();
+	hardware_reset();
+}
+
+// ------ Square Channel Functions ---------------------------------------- */
+// Channel On/Off
+void YMZ294nano3::SetEnable(Channel ch, bool enable)
+{
+	if(ch <=2){
+		if(enable)
+			mixer |= ~(1 << ch); // enable is 0
+		else
+			mixer |= (1 << ch); // disable is 1
+		set_register0(REG_MIXER, mixer);
+	}
+	else{
+		if(enable)
+			mixer |= ~(1 << ch-3); // enable is 0
+		else
+			mixer |= (1 << ch-3); // disable is 1
+		set_register1(REG_MIXER, mixer);
+	}
+}
+
+// Set 12bit Frequency Bit to Channel
+void YMZ294nano3::SetFreqBit(Channel ch, word freqbit)
+{
+	if(ch <=2){
+		sq_freqbits[ch] = freqbit & 0b0000111111111111; // 12bit
+		set_register0(REG_A_L + (ch * 2), sq_freqbits[ch] & 0xff);
+		set_register0(REG_A_H + (ch * 2), (sq_freqbits[ch] >> 8) & 0xff);
+	}
+	else{
+				sq_freqbits[ch-3] = freqbit & 0b0000111111111111; // 12bit
+		set_register1(REG_D_L + ((ch-3) * 2), sq_freqbits[ch-3] & 0xff);
+		set_register1(REG_D_H + ((ch-3) * 2), (sq_freqbits[ch-3] >> 8) & 0xff);
+	}
+
+}
+
+// Set Frequency(Hz) to Channel
+void YMZ294nano3::SetFrequency(Channel ch, word freq)
+{
+	SetFreqBit(ch, (BASE_FREQ / freq));
+}
+
+// Set Volume (4bit)
+void YMZ294nano3::SetVolume(Channel ch, byte volume)
+{
+	if(ch <=2){
+		volumes[ch] = volume;
+		set_register0(REG_VOLUME_A + ch, volumes[ch]);
+	}
+	else{
+		volumes[ch-3] = volume;
+		set_register1(REG_VOLUME_A + ch-3, volumes[ch-3]);
+	}
+}
+
+// ------ Envelope Functions ---------------------------------------- */
+// Set Envelope (Volume Disable)
+void YMZ294nano3::SetEnvEnable(Channel ch, bool enable)
+{
+	if(ch <=2){
+		volumes[ch] = (((enable? 1: 0) << 4) | (volumes[ch] & 0b00001111));
+		set_register(REG_VOLUME_A + ch, volumes[ch]);
+	}
+	else{
+		volumes[ch-3] = (((enable? 1: 0) << 4) | (volumes[ch-3] & 0b00001111));
+		set_register(REG_VOLUME_A + ch-3, volumes[ch-3]);
+	}
+}
+
+// Set Envelope Frequency Bit (16bit)
+void YMZ294nano3::SetEnvFreqBit(word freqbit)
+{
+	env_freqbit = freqbit;
+	set_register(REG_ENV_L, env_freqbit & 0xff);
+	set_register(REG_ENV_H, (env_freqbit >> 8) & 0xff);
+}
+
+// Set Envelope Frequency(Hz)
+void YMZ294nano3::SetEnvFrequency(word freq)
+{
+	SetEnvFreqBit(BASE_FREQ / freq);
+}
+
+// Set Envelope Shape (4bit)
+void YMZ294nano3::SetEnvShape(bool cont, bool att, bool alt, bool hold)
+{
+	env_shape = (cont<< 3) |
+				(att << 2) |
+				(alt << 1) |
+				 hold;
+	set_register(REG_ENV_SHAPE, env_shape);
+}
+
+// ------ Noise Channel Functions ---------------------------------------- */
+// Set 5bit Frequency Bit to Noise Channel
+void YMZ294nano3::SetNoiseFreqBit(byte freqbit)
+{
+	noise_freqbit = freqbit & 0b00011111; // 5bit
+	set_register(REG_NOISE, noise_freqbit);
+}
+
+// Set Frequency(Hz) to Noise Channel
+void YMZ294nano3::SetNoiseFrequency(word freq)
+{
+	SetNoiseFreqBit(BASE_FREQ / freq);
+}
+
+// ------ Mixer Functions ---------------------------------------- */
+void YMZ294nano3::SetMixer(byte noise, byte square)
+{
+	mixer = ((noise & 0b111) << 3) | (square & 0b111);
+	set_register(REG_MIXER, mixer);
+}
+
+/* ------------------------------------------------------------------------ */
+// Private Functions
+/* ------------------------------------------------------------------------ */
+// リセットはYMZ294x2同時に行うため、ch-CDEは省略可能
+/* ------------------------------------------------------------------------ */
+void YMZ294nano3::param_reset(void)
+{
+	set_register(REG_A_L, 0);
+	set_register(REG_A_H, 0);
+	set_register(REG_B_L, 0);
+	set_register(REG_B_H, 0);
+	set_register(REG_C_L, 0);
+	set_register(REG_C_H, 0);
+	set_register(REG_NOISE, 0);
+
+	set_register(REG_MIXER, 0);
+	set_register(REG_VOLUME_A, 0);
+	set_register(REG_VOLUME_B, 0);
+	set_register(REG_VOLUME_C, 0);
+
+	set_register(REG_ENV_L, 0);
+	set_register(REG_ENV_H, 0);
+	set_register(REG_ENV_SHAPE, 0);
+
+	for (int i = 0; i < CH_COUNT; i++){
+		sq_freqbits[i] = 0;
+		volumes[i] = 0;
+	}
+	mixer = 0b00111111; // All Channel Off
+	env_freqbit = 0;
+	env_shape = 0;
+	
+	
+}
+
+void YMZ294nano3::hardware_reset(void)
+{
+    PORTD |= _BV(2);	//digitalWrite(WRCS0_PIN, HIGH);
+    PORTD |= _BV(3);	//digitalWrite(WRCS1_PIN, HIGH);
+    PORTD &= ~_BV(4);	//digitalWrite(A0_PIN, LOW);
+    PORTD &= ~_BV(5);	//digitalWrite(RESET_PIN, LOW);
+	delay(10);
+    PORTD |= _BV(5);	//digitalWrite(RESET_PIN, HIGH);
+}
+
+void YMZ294nano3::set_register(byte addr, byte value)
+{
+  // addr
+    PORTD &= ~_BV(2);	//digitalWrite(WRCS0_PIN, LOW);
+    PORTD &= ~_BV(3);	//digitalWrite(WRCS1_PIN, LOW);
+    PORTD &= ~_BV(4);	//digitalWrite(A0_PIN, LOW);
+  PORTB = addr;
+  PORTC = addr >> 2;
+    PORTD |= _BV(2);	//digitalWrite(WRCS0_PIN, HIGH);
+    PORTD |= _BV(3);	//digitalWrite(WRCS1_PIN, HIGH);
+
+  // value
+    PORTD &= ~_BV(2);	//digitalWrite(WRCS0_PIN, LOW);
+    PORTD &= ~_BV(3);	//digitalWrite(WRCS1_PIN, LOW);
+    PORTD |= _BV(4);	//digitalWrite(A0_PIN, HIGH);
+  PORTB = value;
+  PORTC = value >> 2;
+    PORTD |= _BV(2);	//digitalWrite(WRCS0_PIN, HIGH);
+    PORTD |= _BV(3);	//digitalWrite(WRCS1_PIN, HIGH);
+}
+
+void YMZ294nano3::set_register0(byte addr, byte value)
+{
+  // addr
+    PORTD &= ~_BV(2);	//digitalWrite(WRCS0_PIN, LOW);
+    PORTD &= ~_BV(4);	//digitalWrite(A0_PIN, LOW);
+  PORTB = addr;
+  PORTC = addr >> 2;
+    PORTD |= _BV(2);	//digitalWrite(WRCS0_PIN, HIGH);
+
+  // value
+    PORTD &= ~_BV(2);	//digitalWrite(WRCS0_PIN, LOW);
+    PORTD |= _BV(4);	//digitalWrite(A0_PIN, HIGH);
+  PORTB = value;
+  PORTC = value >> 2;
+    PORTD |= _BV(2);	//digitalWrite(WRCS0_PIN, HIGH);
+}
+
+void YMZ294nano3::set_register1(byte addr, byte value)
+{
+  // addr
+    PORTD &= ~_BV(3);	//digitalWrite(WRCS1_PIN, LOW);
+    PORTD &= ~_BV(4);	//digitalWrite(A0_PIN, LOW);
+  PORTB = addr;
+  PORTC = addr >> 2;
+    PORTD |= _BV(3);	//digitalWrite(WRCS1_PIN, HIGH);
+
+  // value
+    PORTD &= ~_BV(3);	//digitalWrite(WRCS1_PIN, LOW);
+    PORTD |= _BV(4);	//digitalWrite(A0_PIN, HIGH);
+  PORTB = value;
+  PORTC = value >> 2;
+    PORTD |= _BV(3);	//digitalWrite(WRCS1_PIN, HIGH);
+}
+
---
+++ nano_MIDI_YMZ294_DUAL/libraries/YMZ294nano3/YMZ294nano3.h
@@ -0,0 +1,139 @@
+/*
+インパクトドリルさんのYMZ294ライブラリ
+http://wp.hrmux.com/?page_id=991
+を修正し、MIDI対応、YMZ294x2個対応しています。
+Ver3:高速化のため全てポート制御に変更
+*/
+
+#ifndef YMZ294nano3_H
+#define YMZ294nano3_H
+
+#include "Arduino.h"
+
+// YMZ294nano3 Registers
+#define	REG_A_L	0x00
+#define	REG_A_H	0x01
+#define	REG_B_L	0x02
+#define	REG_B_H	0x03
+#define	REG_C_L	0x04
+#define	REG_C_H	0x05
+#define	REG_NOISE	0x06
+#define	REG_D_L	0x00
+#define	REG_D_H	0x01
+#define	REG_E_L	0x02
+#define	REG_E_H	0x03
+#define	REG_F_L	0x04
+#define	REG_F_H	0x05
+
+#define	REG_MIXER	0x07
+#define	REG_VOLUME_A	0x08
+#define	REG_VOLUME_B	0x09
+#define	REG_VOLUME_C	0x0a
+#define	REG_VOLUME_D	0x08
+#define	REG_VOLUME_E	0x09
+#define	REG_VOLUME_F	0x0a
+
+
+#define	REG_ENV_L	0x0b
+#define	REG_ENV_H	0x0c
+#define	REG_ENV_SHAPE	0x0d
+
+// YMZ294nano3 Channels
+enum Channel{
+  CH_A = 0,
+  CH_B = 1,
+  CH_C = 2,
+  CH_D = 3,
+  CH_E = 4,
+  CH_F = 5,
+};
+const byte CH_COUNT = 3;
+
+//#define BASE_FREQ 125000
+#define BASE_FREQ 167000
+
+// MML char to FreqBit Convert Table
+const int noteFreq[] = {
+
+//  0,  9,  9, 10,  10, 11,   12,	12,	13,	14,	15,	15,
+//  16,  17, 18, 19,  21, 22,  23,	25,	26,	28,	29,	31,
+//  33,  35, 37,	39,  41, 44,  46,	49,	52,	55,	58,	62,
+  65,	69, 73,	78,  41, 44,  46,	49,	52,	55,	58,	62,
+  65,	69, 73,	78,  41, 44,  46,	49,	52,	55,	58,	62,
+  65,	69, 73,	78,  41, 44,  46,	49,	52,	55,	58,	62,
+  65,	69, 73,	78,  82, 87,  93,	98,	104,	110,	117,	124,
+  131, 139, 147, 156, 165, 175, 185,	196,	208,	220,	233,	247,
+  262, 277, 294, 311, 330, 349, 370,	392,	415,	440,	466,	494,
+  523, 554, 587, 622, 659, 699, 740,	784,	831,	880,	932,	988,
+  1047, 1109, 1175, 1245, 1319, 1397,	1480,	1568,	1661,	1760,	1865,	1976,
+  2093, 2218, 2349, 2489, 2637,	2794,	2960,	3136,	3322,	3520,	3729,	3951,
+  4186, 4435, 4699, 4978, 5274,	5587,	5920,	6272,	6645,	7040,	7459,	7902,
+  8372, 8870, 9397, 9956, 10548,11175,	11840,	12544
+};
+
+// MML char to Freqyency Convert Function
+word CalcFreqByMML(byte octave, char note, bool is_sharp = false);
+
+class YMZ294nano3 {
+public:
+		YMZ294nano3(byte wrcs0_pin, byte wrcs1_pin, byte a0_pin, byte reset_pin);
+
+	void Reset(void);
+	
+	// Square Wave
+	void SetEnable(Channel channel, bool enable);
+	void SetFreqBit(Channel ch, word freqbit);
+	void SetFrequency(Channel ch, word freq);
+	void SetVolume(Channel ch, byte Volume);
+
+	// Envelope
+	void SetEnvEnable(Channel ch, bool enable);
+	void SetEnvFreqBit(word freq);
+	void SetEnvFrequency(word freq);
+	void SetEnvShape(bool cont, bool att, bool alt, bool hold);
+
+	// Noise Channel
+//	void SetNoiseEnable(bool enable);
+	void SetNoiseFreqBit(byte freqbit);
+	void SetNoiseFrequency(word freq);
+
+	// Set Mixer Channel
+	void SetMixer(byte noisemsk, byte tonemsk);
+
+	// Get Parameter
+	word FreqBit(Channel ch){return sq_freqbits[ch];}
+	byte NoiseFreqBit(void){return noise_freqbit;}
+	byte Mixer(void){return mixer;}
+	byte Volume(byte ch){return volumes[ch];}
+	word EnvFreqBit(void){return env_freqbit;}
+	byte EnvShape(void){return env_shape;}
+
+	// Get Pin No.
+	byte GetWRCS0(){return WRCS0_PIN;}
+	byte GetWRCS1(){return WRCS1_PIN;}
+	byte GetA0(){return A0_PIN;}
+	byte GetRESET(){return RESET_PIN;}
+	
+private:
+	// Arduino Pins
+	const byte WRCS0_PIN;
+	const byte WRCS1_PIN;
+	const byte A0_PIN;
+	const byte RESET_PIN;
+	
+	// YMZ294nano3 Parameters
+	word sq_freqbits[CH_COUNT];	// Square Wave Frequency Information A/B/C (12bit * 3)
+	byte noise_freqbit;			// Noise Frequency Information (5bit)
+	byte mixer;					// Mixer Status (6bit)
+	byte volumes[CH_COUNT];		// Volume Status (5bit * 3)
+	word env_freqbit;			// Envelope Frequency Information (16bit)
+	byte env_shape;				// Envelope Shape (4bit)
+	
+	void param_reset(void);
+	void hardware_reset(void);
+	void set_register(byte addr, byte value);
+	void set_register0(byte addr, byte value);
+	void set_register1(byte addr, byte value);
+};
+
+#endif
